931b68598de77766faa1331f73d98f360aa96ef5
Kyle Mahan <kyle.mahan@gmail.com>
2015-07-03 12:22:03 -0700

    add POST form for test suite

--- src/main/java/com/kylewm/mf2j/ParsingResource.java
+++ src/main/java/com/kylewm/mf2j/ParsingResource.java
@@ -5,7 +5,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -22,16 +24,35 @@ import com.google.common.collect.ImmutableMap;
 public class ParsingResource {
 
     public ParsingResource() {
-        
+
     }
-    
+
+    @POST
+    public String parse(@FormParam("url") String url, @FormParam("html") String html) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter printer = mapper.writerWithDefaultPrettyPrinter();
+        try {
+            Mf2Parser parser = new Mf2Parser();
+            try {
+                return printer.writeValueAsString(parser.parse(html, new URI(url)));
+            } catch (URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return printer.writeValueAsString(ImmutableMap.of("error", (Object) e.getMessage()));
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return String.format("{\"error\": \"%s\"}", e.getMessage().replace("\"", "\\\""));
+        }
+    }
+
     @GET
     public String parse(@QueryParam("url") Optional<String> url) {
         ObjectMapper mapper = new ObjectMapper();
         ObjectWriter printer = mapper.writerWithDefaultPrettyPrinter();
         try {
             if (url.isPresent()) {
-                Parser parser = new com.kylewm.mf2j.Parser();
+                Mf2Parser parser = new Mf2Parser();
                 try {
                     return printer.writeValueAsString(parser.parse(new URI(url.get())));
                 } catch (IOException e) {
@@ -50,5 +71,5 @@ public class ParsingResource {
             return String.format("{\"error\": \"%s\"}", e.getMessage().replace("\"", "\\\""));
         }
     }
-    
+
 }
